9663640a79f7312a4142d2abd6117f59c02aa1a3
madayath03 <harikrishnanm7@gmail.com>
2023-07-11 23:35:19 +0530

    get added

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
+import { appendFile } from "fs";
 
 // comfiguration
 const __filename = fileURLToPath(import.meta.url);
@@ -31,6 +32,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+app.get('/', (req, res) => {
+    res.send('social server alive!')
+})
 
 // file storage
 const storage = multer.diskStorage({
